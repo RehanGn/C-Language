0f16397567ce0579bf0cbfde5be84ca01736650f
RehanGn <94187098+RehanGn@users.noreply.github.com>
2022-03-24 12:03:35 +0530

    Add files via upload
    Size of any number

---
+++ size.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    int y;
+    int z;
+    x = sizeof (char);
+    y = sizeof(float);
+    z = sizeof(double);
+    printf("%d\n", x);
+    printf("%d\n", y);
+    printf("%d\n", z);
+}
