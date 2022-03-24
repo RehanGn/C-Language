bfc1241c475fb86859e5e913742c51ee36ec2d1f
RehanGn <94187098+RehanGn@users.noreply.github.com>
2022-03-24 11:57:31 +0530

    Add files via upload
    Find the module

---
+++ module.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    int y;
+    int z;
+    double a;
+    x = 202%10;
+    y = 33/10;
+    z = 434 + 34353;
+    a = 433 * 33;
+    printf("%d\n", x);
+    printf("%d\n", y);
+    printf("%d\n", z);
+    printf("%lf\n", a);
+}
