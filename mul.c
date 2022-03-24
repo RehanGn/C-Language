f587b296503c6b45d6b73875e9b8e42bea3cbe45
RehanGn <94187098+RehanGn@users.noreply.github.com>
2022-03-24 12:00:34 +0530

    Add files via upload
    find the two no multiplication

--- mul.c
+++ mul.c
@@ -1,10 +1,9 @@
-#include <stdio.h>
-int main() {
-  int n, i;
-  printf("Enter an integer: ");
-  scanf("%d", &n);
-  for (i = 1; i <= 10; ++i) {
-    printf("%d * %d = %d \n", n, i, n * i);
-  }
-  return 0;
+#include<stdio.h>
+int main()
+{
+    double x, y, product;
+    printf("Enter the two no: ");
+    scanf("%lf %lf", &x, &y);
+    product = x * y;
+    printf("Product = %.2lf", product);
 }
